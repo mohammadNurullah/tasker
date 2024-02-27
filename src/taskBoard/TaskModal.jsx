4c8369a311de02e7e6cb3777dbb2fc1ed6cb19dc
mohammadNurullah <nurullahsarkar963@gmail.com>
2024-02-28 01:55:21 +0600

    initial commit

--- src/taskBoard/TaskModal.jsx
+++ src/taskBoard/TaskModal.jsx
@@ -30,7 +30,7 @@ export default function TaskModal({ onSave, taskToUpdate, onCancle }) {
   return (
     <>
       <div className=" bg-black bg-opacity-70 w-full h-full absolute top-0 left-0 z-10"></div>
-      <form className="mx-auto my-10 w-full max-w-[740px] rounded-xl border border-[#FEFBFB]/[36%] bg-[#191D26] p-9 max-md:px-4 lg:my-20 lg:p-11 z-10 absolute top-1/4 left-1/3">
+      <form className="mx-auto my-10 w-full max-w-[740px] rounded-xl border border-[#FEFBFB]/[36%] bg-[#191D26] p-9 max-md:px-4 lg:my-20 lg:p-11 z-10 absolute -top-[10%] left-1/3">
         <h2 className="mb-9 text-center text-2xl font-bold text-white lg:mb-11 lg:text-[28px]">
           {isAdd ? "Add New Task" : "Edit Task"}
         </h2>
